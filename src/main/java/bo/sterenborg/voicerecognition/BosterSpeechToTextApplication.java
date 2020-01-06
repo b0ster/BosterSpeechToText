d76debeb2cb096c6f5d044c6a0f2532b61ea94b3
Bo Sterenborg <Bo.Sterenborg@hva.nl>
2020-01-06 12:25:09 +0100

    Added multi/single file choice

--- src/main/java/bo/sterenborg/voicerecognition/BosterSpeechToTextApplication.java
+++ src/main/java/bo/sterenborg/voicerecognition/BosterSpeechToTextApplication.java
@@ -11,23 +11,36 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Scanner;
 
 public class BosterSpeechToTextApplication {
     private static final String RESULTS_DIRECTORY = "results/";
 
     public static void main(String[] args)  {
+        final Scanner scanner = new Scanner(System.in);
         final FileSpeechToTextConverter converter = FileSpeechToTextConverter.newInstance();
         final SpeechToTextResults speechToTextResults = new SpeechToTextResults();
         final List<SpeechToTextResult> speechToTextResultList = new ArrayList<>();
-        for (String audioFileName : AudioUtils.getAllOriginalAudioFileNames()) {
-            final SpeechToTextResult speechToTextResult = converter.convertSpeechToText(audioFileName);
+        System.out.print("Would you like to use all file or one specific file? (A = all, S = specific): ");
+        final char choice = scanner.next().charAt(0);
+        if (Character.toLowerCase(choice) == 'a') {
+            for (String audioFileName : AudioUtils.getAllOriginalAudioFileNames()) {
+                final SpeechToTextResult speechToTextResult = converter.convertSpeechToText(audioFileName);
+                speechToTextResultList.add(speechToTextResult);
+                logResult(speechToTextResult, speechToTextResult.getFileName());
+                saveAsCSV(speechToTextResult, speechToTextResult.getFileName());
+            }
+            speechToTextResultList.forEach(speechToTextResults::addSpeechToTextResult);
+            logResult(speechToTextResults, "combined");
+            saveAsCSV(speechToTextResults, "combined");
+        }else{
+            System.out.print("Which file would you like to use? (from /audiofiles/original): ");
+            final String fileName = scanner.next();
+            final SpeechToTextResult speechToTextResult = converter.convertSpeechToText(fileName);
             speechToTextResultList.add(speechToTextResult);
             logResult(speechToTextResult, speechToTextResult.getFileName());
             saveAsCSV(speechToTextResult, speechToTextResult.getFileName());
         }
-        speechToTextResultList.forEach(speechToTextResults::addSpeechToTextResult);
-        logResult(speechToTextResults, "combined");
-        saveAsCSV(speechToTextResults, "combined");
     }
 
     private static void logResult(Object result, String fileName) {
